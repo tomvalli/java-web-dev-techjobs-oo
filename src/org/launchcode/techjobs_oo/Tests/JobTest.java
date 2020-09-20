ad4b75c0f7f8907020737e220842bcbdf8ebc09f
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-09-19 22:27:14 -0500

    Added JUnit tests for Job class

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,51 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+    Job job1, job2;
+
+    @Before
+    public void createJobs() {
+        job1 = new Job();
+        job2 = new Job();
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertEquals(job1.getId(), (job2.getId()-1), 0);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job3.getName() instanceof String);
+        assertEquals(job3.getName(), "Product tester");
+
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertEquals(job3.getEmployer().getValue(), "ACME");
+
+        assertTrue(job3.getLocation() instanceof Location);
+        assertEquals(job3.getLocation().getValue(), "Desert");
+
+        assertTrue(job3.getPositionType() instanceof PositionType);
+        assertEquals(job3.getPositionType().getValue(), "Quality control");
+
+        assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(job3.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(job4.equals(job5));
+    }
+
+
+}
